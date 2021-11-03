441ffdc111b5e2520ef8e7ee7bf1385a1f194222
stefrie <sriehle@gmail.com>
2021-11-02 18:29:43 -0700

    code challenge 12

---
+++ javascript/stack-and-queue/animal-shelter.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const { Animal, AnimalShelter } = require('./animal-shelter.js');
+
+describe('Animal Shelter', () => {
+	test('can successfully add an animal to the queue', () => {
+		const q = n
+	})
+});
